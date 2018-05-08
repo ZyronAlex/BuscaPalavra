3234f78ffc8a80ee101146c2ec903ede8620717e
AlexFBelo <fraga040506>
2018-05-08 17:26:38 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/computacaoognitiva/buscapalavra/PontuacaoActivity.java
@@ -0,0 +1,13 @@
+package com.computacaoognitiva.buscapalavra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PontuacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pontuacao);
+    }
+}
